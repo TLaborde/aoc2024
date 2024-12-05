25d64a838c9689518bf12ef08293a4c3f4267d5e
Thomas Laborde <thomas.laborde@nnlife.co.jp>
2024-12-05 05:51:08 +0000

    rewritten nicerly

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,82 +2,63 @@ advent_of_code::solution!(5);
 use std::cmp::Ordering;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    // split input on the empty line
-    let mut parts = input.split("\n\n");
-    // for each line in the first part, split on | to get 2 integers
-    let rules = parts.next().unwrap().lines().map(|line| {
-        let mut parts = line.split("|");
-        let page_before = parts.next().unwrap().parse::<u32>().unwrap();
-        let page_after = parts.next().unwrap().parse::<u32>().unwrap();
-        (page_before, page_after)
-    }).collect::<Vec<(u32, u32)>>();
-    // the second part are array of integet separated by comma
-    let manuals = parts.next().unwrap().lines().collect::<Vec<&str>>();
-    // for each manual, split on comma and parse each integer
-    let manual_pages = manuals.iter().map(|manual| {
-        manual.split(",").map(|page| page.parse::<u32>().unwrap()).collect::<Vec<u32>>()
-    }).collect::<Vec<Vec<u32>>>();
+    let (rules, manual_pages) = parse_input(input);
 
-    // for each manual, if the index and rule are in the array, the index need to be before rule
-    let mut valid = 0;
-    'manual_loop: for manual in manual_pages.iter() {
-        for rule in rules.iter() {
-            if manual.contains(&rule.0) && manual.contains(&rule.1) {
-                if manual.iter().position(|&x| x == rule.0).unwrap() < manual.iter().position(|&x| x == rule.1).unwrap() {
-                    continue;
-                } else {
-                    // go to next manual
-                    continue 'manual_loop;
-                }
-            }
-        }
-        // add to valid the value in the middle of the manual array
-        valid += manual[manual.len() / 2];
-    }
-    //
+    let valid = manual_pages.iter()
+        .filter(|manual| {
+            let mut sorted = (*manual).clone();
+            sorted.sort_by(|a, b| sort_manual_pages(a, b, &rules));
+            sorted == **manual
+        })
+        .map(|manual| manual[manual.len() / 2])
+        .sum();
     Some(valid)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let (rules, manual_pages) = parse_input(input);
+
+    let valid = manual_pages.iter()
+        .map(|manual| {
+            let mut sorted = (*manual).clone();
+            sorted.sort_by(|a, b| sort_manual_pages(a, b, &rules));
+            if sorted != **manual {
+                sorted[sorted.len() / 2]
+            } else {
+                0
+            }
+        })
+        .sum();
+    Some(valid)
+}
+
+fn parse_input(input: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let mut parts = input.split("\n\n");
-    // for each line in the first part, split on | to get 2 integers
     let rules = parts.next().unwrap().lines().map(|line| {
-        let mut parts = line.split("|");
+        let mut parts = line.split('|');
         let page_before = parts.next().unwrap().parse::<u32>().unwrap();
         let page_after = parts.next().unwrap().parse::<u32>().unwrap();
         (page_before, page_after)
     }).collect::<Vec<(u32, u32)>>();
 
-    // create a sorting function that will sort the manual pages based on the rules
-    let sort = |a: &u32, b: &u32| {
-            for rule in rules.iter() {
-                if a == &rule.0 && b == &rule.1 {
-                    return Ordering::Less;
-                }
-                if a == &rule.1 && b == &rule.0 {
-                    return Ordering::Greater;
-                }
-            }
-            Ordering::Equal
-        };
-
-    // the second part are array of integet separated by comma
     let manuals = parts.next().unwrap().lines().collect::<Vec<&str>>();
-    // for each manual, split on comma and parse each integer
     let manual_pages = manuals.iter().map(|manual| {
-        manual.split(",").map(|page| page.parse::<u32>().unwrap()).collect::<Vec<u32>>()
+        manual.split(',').map(|page| page.parse::<u32>().unwrap()).collect::<Vec<u32>>()
     }).collect::<Vec<Vec<u32>>>();
 
-    // sort each manual, compare the sorted manual with the original manual
-    let mut valid = 0;
-    for manual in manual_pages.iter() {
-        let mut sorted = manual.clone();
-        sorted.sort_by(sort);
-        if sorted != *manual {
-            valid += sorted[sorted.len() / 2];
+    (rules, manual_pages)
+}
+
+fn sort_manual_pages(a: &u32, b: &u32, rules: &[(u32, u32)]) -> Ordering {
+    for rule in rules.iter() {
+        if a == &rule.0 && b == &rule.1 {
+            return Ordering::Less;
+        }
+        if a == &rule.1 && b == &rule.0 {
+            return Ordering::Greater;
         }
     }
-    Some(valid)
+    Ordering::Equal
 }
 
 #[cfg(test)]
