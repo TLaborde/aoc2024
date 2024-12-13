89163b871beab47e31bdbb8958e5f5d6533e0477
Thomas Laborde <thomas.laborde@nnlife.co.jp>
2024-12-13 01:17:29 +0000

    rewrite

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,74 +1,60 @@
 use std::cell;
 
 advent_of_code::solution!(12);
-
 pub fn part_one(input: &str) -> Option<u32> {
-    //parse input as 2d grid of characters
-    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    // create hashmap to store for each coordinate the number of fence needed around
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let mut fence_map = std::collections::HashMap::new();
-    // iterate over the grid
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            let cell = grid[y][x];
-            // count the number of fence needed for each cell up down left right
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, &cell) in row.iter().enumerate() {
             let mut fence_count = 0;
-                if y == 0 || grid[y - 1][x] != cell {
-                    fence_count += 1;
-                }
-                if y == grid.len() - 1 || grid[y + 1][x] != cell {
-                    fence_count += 1;
-                }
-                if x == 0 || grid[y][x - 1] != cell {
-                    fence_count += 1;
-                }
-                if x == grid[y].len() - 1 || grid[y][x + 1] != cell {
-                    fence_count += 1;
-                }
+            if y == 0 || grid[y - 1][x] != cell {
+                fence_count += 1;
+            }
+            if y == grid.len() - 1 || grid[y + 1][x] != cell {
+                fence_count += 1;
+            }
+            if x == 0 || grid[y][x - 1] != cell {
+                fence_count += 1;
+            }
+            if x == row.len() - 1 || grid[y][x + 1] != cell {
+                fence_count += 1;
+            }
             fence_map.insert((x, y), fence_count);
         }
     }
-    // create an empty hashset of visited coordinates
+
     let visited = cell::RefCell::new(std::collections::HashSet::new());
-    // create a function to recursively visit all the cells of the same color
-    // iterate over the grid
     let mut sum = 0;
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            // if the cell has not been visited
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _) in row.iter().enumerate() {
             if !visited.borrow().contains(&(x, y)) {
-                // create a stack of coordinates to visit
                 let mut stack = vec![(x, y)];
-                // create a variable to store the number of cells visited
                 let mut area = 0;
                 let mut fences = 0;
-                // while the stack is not empty
+
                 while let Some((x, y)) = stack.pop() {
-                    // if the cell has not been visited
                     if !visited.borrow().contains(&(x, y)) {
-                        // mark the cell as visited
                         visited.borrow_mut().insert((x, y));
-                        // increment the count
                         area += 1;
-                        // get the number of fence needed for the cell
-                        fences += fence_map.get(&(x, y)).unwrap();
+                        fences += fence_map[&(x, y)];
                         let cell = grid[y][x];
-                        // add the neighbors to the stack
-                        if x > 0 && grid[y][x - 1] == cell{
+
+                        if x > 0 && grid[y][x - 1] == cell {
                             stack.push((x - 1, y));
                         }
-                        if x < grid[y].len() - 1 && grid[y][x+1] == cell{
+                        if x < row.len() - 1 && grid[y][x + 1] == cell {
                             stack.push((x + 1, y));
                         }
-                        if y > 0 && grid[y-1][x] == cell{
+                        if y > 0 && grid[y - 1][x] == cell {
                             stack.push((x, y - 1));
                         }
-                        if y < grid.len() - 1 && grid[y +1][x] == cell{
+                        if y < grid.len() - 1 && grid[y + 1][x] == cell {
                             stack.push((x, y + 1));
                         }
                     }
                 }
-                // print the area and fence count
                 sum += area * fences;
             }
         }
@@ -77,110 +63,87 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    //parse input as 2d grid of characters
-    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    // create an empty hashset of visited coordinates
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let visited = cell::RefCell::new(std::collections::HashSet::new());
-    // create a function to recursively visit all the cells of the same color
-    // iterate over the grid
     let mut sum = 0;
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            // if the cell has not been visited
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, &cell) in row.iter().enumerate() {
             if !visited.borrow().contains(&(x, y)) {
-                // create a stack of coordinates to visit
                 let mut stack = vec![(x, y)];
-                // create a variable to store the number of cells visited
                 let mut subset = std::collections::HashSet::new();
                 let mut corners = 0;
-                // while the stack is not empty
+
                 while let Some((x, y)) = stack.pop() {
-                    // if the cell has not been visited
                     if !visited.borrow().contains(&(x, y)) {
-                        // mark the cell as visited
                         visited.borrow_mut().insert((x, y));
-                        // increment the count
                         subset.insert((x, y));
-                        let cell = grid[y][x];
-                        // add the neighbors to the stack
-                        if x > 0 && grid[y][x - 1] == cell{
+
+                        if x > 0 && grid[y][x - 1] == cell {
                             stack.push((x - 1, y));
                         }
-                        if x < grid[y].len() - 1 && grid[y][x+1] == cell{
+                        if x < row.len() - 1 && grid[y][x + 1] == cell {
                             stack.push((x + 1, y));
                         }
-
-                        if y > 0 && grid[y-1][x] == cell{
+                        if y > 0 && grid[y - 1][x] == cell {
                             stack.push((x, y - 1));
                         }
-                        if y < grid.len() - 1 && grid[y +1][x] == cell{
+                        if y < grid.len() - 1 && grid[y + 1][x] == cell {
                             stack.push((x, y + 1));
                         }
                     }
                 }
-                for cell in subset.iter() {
-                    let x = cell.0;
-                    let y = cell.1;
+
+                for &(x, y) in &subset {
                     let mut missing_neighbors = 0;
-                    if x == 0 {
+                    if x == 0 || !subset.contains(&(x - 1, y)) {
                         missing_neighbors += 1;
-                    } else if x > 0 && !subset.contains(&(x - 1, y)) {
+                    }
+                    if x == row.len() - 1 || !subset.contains(&(x + 1, y)) {
+                        missing_neighbors += 1;
+                    }
+                    if y == 0 || !subset.contains(&(x, y - 1)) {
+                        missing_neighbors += 1;
+                    }
+                    if y == grid.len() - 1 || !subset.contains(&(x, y + 1)) {
                         missing_neighbors += 1;
                     }
 
-                    if x == grid[y].len() - 1 {
-                        missing_neighbors += 1;
-                    } else if x < grid[y].len() - 1 && !subset.contains(&(x + 1, y)) {
-                        missing_neighbors += 1;
-                    }
-
-                    if y == 0 {
-                        missing_neighbors += 1;
-                    } else if y > 0 && !subset.contains(&(x, y - 1)) {
-                        missing_neighbors += 1;
-                    }
-
-                    if y == grid.len() - 1 {
-                        missing_neighbors += 1;
-                    } else if y < grid.len() - 1 && !subset.contains(&(x, y + 1)) {
-                        missing_neighbors += 1;
-                    }
                     let corners_add = match missing_neighbors {
                         4 => 4,
                         3 => 2,
-                        2 => if (x > 0 && subset.contains(&(x - 1, y)) && x < grid[y].len() - 1 && subset.contains(&(x + 1, y))) ||
-                                (y > 0 && subset.contains(&(x, y - 1)) && y < grid.len() - 1 && subset.contains(&(x, y + 1)) ){ 0 } else {1},
+                        2 => {
+                            if (x > 0 && subset.contains(&(x - 1, y)) && x < row.len() - 1 && subset.contains(&(x + 1, y)))
+                                || (y > 0 && subset.contains(&(x, y - 1)) && y < grid.len() - 1 && subset.contains(&(x, y + 1)))
+                            {
+                                0
+                            } else {
+                                1
+                            }
+                        }
                         _ => 0,
                     };
-                    println!("x: {}, y: {}, missing_neighbors: {}, corners_add: {}", x, y, missing_neighbors, corners_add);
                     corners += corners_add;
 
-                    if x > 0 && y > 0 && !subset.contains(&(x - 1, y - 1)) && subset.contains(&(x, y - 1)) && subset.contains(&(x - 1, y))  {
-                        println!("x: {}, y: {}", x, y);
+                    if x > 0 && y > 0 && !subset.contains(&(x - 1, y - 1)) && subset.contains(&(x, y - 1)) && subset.contains(&(x - 1, y)) {
                         corners += 1;
                     }
-                    if x < grid[y].len() - 1 &&  y < grid.len() - 1 &&!subset.contains(&(x + 1, y + 1)) && subset.contains(&(x, y + 1)) && subset.contains(&(x + 1, y))  {
-                        println!("x: {}, y: {}", x, y);
+                    if x < row.len() - 1 && y < grid.len() - 1 && !subset.contains(&(x + 1, y + 1)) && subset.contains(&(x, y + 1)) && subset.contains(&(x + 1, y)) {
                         corners += 1;
                     }
-                    if x > 0 && y < grid.len() - 1 && !subset.contains(&(x - 1, y + 1)) && subset.contains(&(x, y + 1)) && subset.contains(&(x - 1, y))  {
-                        println!("x: {}, y: {}", x, y);
+                    if x > 0 && y < grid.len() - 1 && !subset.contains(&(x - 1, y + 1)) && subset.contains(&(x, y + 1)) && subset.contains(&(x - 1, y)) {
                         corners += 1;
                     }
-                    if x < grid[y].len() - 1 && y > 0 && !subset.contains(&(x + 1, y - 1)) && subset.contains(&(x, y - 1)) && subset.contains(&(x + 1, y))  {
-                        println!("x: {}, y: {}", x, y);
+                    if x < row.len() - 1 && y > 0 && !subset.contains(&(x + 1, y - 1)) && subset.contains(&(x, y - 1)) && subset.contains(&(x + 1, y)) {
                         corners += 1;
                     }
                 }
 
-                // print the subset len and corners count
-                println!("subset: {:?}, corners: {}", subset, corners);
                 sum += subset.len() * corners;
             }
         }
     }
     Some(sum as u32)
-
 }
 
 #[cfg(test)]
